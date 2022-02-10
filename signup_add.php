5c73d7b680042a9ee34754799fb2616db12152b6
yahya1403 <yahyamd1403@gmail.com>
2022-02-10 21:44:04 +0530

    sign up changes

--- signup_add.php
+++ signup_add.php
@@ -10,13 +10,17 @@ if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
   echo "Invalid Secret code";
 }else{
   include_once('db/config.php');
-  $sql = "INSERT INTO user_list (user_name, pass, secret_code)
-  VALUES ('".$email."', '".$pass."', '".$secret."')";
-  
-  if (mysqli_query($conn, $sql)) {
-    echo "Registered successfully";
-  } else {
-    echo "some details are not matched";
-  }
+  $s = "SELECT user_name FROM user_list WHERE user_name='".$email."'";
+  $result = mysqli_query($conn, $s);
+    if (mysqli_num_rows($result) == 0) {
+            $sql = "INSERT INTO user_list (user_name, pass, secret_code)
+            VALUES ('".$email."', '".$pass."', '".$secret."')";
+            if (mysqli_query($conn, $sql)) {
+              echo "Registered successfully";
+                    } 
+    }
+    else{
+      echo "User already added";
+    }
 }
 ?>
